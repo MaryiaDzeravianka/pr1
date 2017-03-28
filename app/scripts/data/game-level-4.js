a09e4f780d51ea23f96133c5da8d8d00e3680fce
MaryiaDzeravianka <bondarenkam@gmail.com>
2017-03-29 00:55:27 +0300

    add gulp

---
+++ app/scripts/data/game-level-4.js
@@ -0,0 +1,72 @@
+var gameDataLevel4 = [{
+        'primer': '2&times;1=?',
+        'answers': [2, 3, 1, 4]
+},
+    {
+        'primer': '2&times;2=?',
+        'answers': [4, 5, 6, 3]
+},
+    {
+        'primer': '2&times;3=?',
+        'answers': [6, 7, 5, 4]
+},
+    {
+        'primer': '2&times;4=?',
+        'answers': [8, 9, 6, 2]
+},
+    {
+        'primer': '2&times;5=?',
+        'answers': [10, 9, 3, 6]
+},
+    {
+        'primer': '2&times;6=?',
+        'answers': [12, 9, 8, 10]
+},
+    {
+        'primer': '2&times;7=?',
+        'answers': [14, 8, 9, 10]
+},
+    {
+        'primer': '2&times;8=?',
+        'answers': [16, 11, 10, 13]
+},
+    {
+        'primer': '2&times;9=?',
+        'answers': [18, 11, 9, 13]
+},
+    {
+        'primer': '2&times;10=?',
+        'answers': [20, 11, 12, 13]
+},
+    {
+        'primer': '3&times;2=?',
+        'answers': [6, 5, 9, 1]
+},
+    {
+        'primer': '3&times;3=?',
+        'answers': [9, 6, 7, 8]
+},
+    {
+        'primer': '3&times;4=?',
+        'answers': [12, 7, 8, 1]
+},
+    {
+        'primer': '3&times;5=?',
+        'answers': [15, 8, 7, 2]
+},
+    {
+        'primer': '3&times;6=?',
+        'answers': [18, 9, 3, 7]
+},
+    {
+        'primer': '3&times;7=?',
+        'answers': [21, 11, 9, 10]
+},
+    {
+        'primer': '3&times;8=?',
+        'answers': [24, 11, 8, 9]
+},
+    {
+        'primer': '3&times;9=?',
+        'answers': [27, 11, 12, 10]
+}];
